782365d5f921f8fa46894eb2ab8f2193756837aa
mxculexerRyan <mxculexer@gmail.com>
2022-04-07 17:00:01 +0300

    changing the pass-show directory

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-        <script src="./js/pass-show-hide.js"></script>
+        <script src="./js/password/pass-show-hide.js"></script>
         <script src="./js/dark-mode/dark-mode-switch.min.js"></script>
     </body>   
 </html>
