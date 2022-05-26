9d6974468434fea2e972c5c4008d6e5497d087c2
mxculexerRyan <79500662+mxculexerRyan@users.noreply.github.com>
2022-05-26 21:16:23 +0300

    Updated Group message include sender & time

--- php/groups/get_chat.php
+++ php/groups/get_chat.php
@@ -6,20 +6,31 @@
         $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
         $output = "";
 
-        $sql = mysqli_query($conn, "SELECT * FROM group_messages WHERE (receiver_id = '{$incoming_id}')");
+        // $sql = mysqli_query($conn, "SELECT * FROM group_messages WHERE (receiver_id = '{$incoming_id}')");
+        $sql = mysqli_query($conn, "SELECT gmsg_id, sender_id, receiver_id, msg, send_time, receive_time, 
+            users.Username AS uname, users.u_id FROM group_messages, users WHERE ((sender_id = users.u_id)
+            && (receiver_id = '{$incoming_id}')) ORDER BY gmsg_id ASC");
+
         if(mysqli_num_rows($sql) > 0){
             while($row = mysqli_fetch_assoc($sql)){
+                $time = substr($row['send_time'],11,5);
                 if($row['sender_id'] === $outgoing_id){
                     $output .= '<div class="chat outgoing">
                                     <div class="details">
-                                        <p>'.$row['msg'].'</p>
+                                        <p>'.$row['msg'].'<br>
+                                            <span class="out-time">'.$time.'</span>
+                                        </p>
                                     </div>
                                 </div>';
                 }else{
                     $output .= '<div class="chat incoming">
                                     <img src="./images/zai.jpg" alt="">
                                     <div class="details">
-                                        <p><span>Ryan: </span><br>'.$row['msg'].'</p>
+                                        <p>
+                                            <b><span>'.$row['uname'].': </span></b>
+                                            <br>'.$row['msg'].'<br>
+                                            <span class="out-time">'.$time.'</span>
+                                        </p>
                                     </div>
                                 </div>';
                 }
